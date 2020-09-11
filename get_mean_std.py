6042c6bcbc8534996511f19ed874fd486896b1d8
justHungryMan <justHungryMan@gmail.com>
2020-09-11 18:05:20 +0900

    [ADD] Code

---
+++ get_mean_std.py
@@ -0,0 +1,42 @@
+import torch
+import torchvision
+
+class MyDataset(Dataset):
+    def __init__(self):
+        self.data = torch.randn(1000, 3, 224, 224)
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        return x
+
+    def __len__(self):
+        return len(self.data)
+
+def main():
+    device = torch.device("cuda")
+    dataset = MyDataset()
+
+    start = timeit.time.perf_counter()
+    data = dataset.data.to(device)
+    print("Mean:", torch.mean(data, dim=(0, 2, 3)))
+    print("Std:", torch.std(data, dim=(0, 2, 3)))
+    print("Elapsed time: %.3f seconds" % (timeit.time.perf_counter() - start))
+    print()
+
+    start = timeit.time.perf_counter()
+    mean = 0.
+    for data in dataset:
+        data = data.to(device)
+        mean += torch.mean(data, dim=(1, 2))
+    mean /= len(dataset)
+    print("Mean:", mean)
+
+    temp = 0.
+    nb_samples = 0.
+    for data in dataset:
+        data = data.to(device)
+        temp += ((data.view(3, -1) - mean.unsqueeze(1)) ** 2).sum(dim=1)
+        nb_samples += np.prod(data.size()[1:])
+    std = torch.sqrt(temp/nb_samples)
+    print("Std:", std)
+    print("Elapsed time: %.3f seconds" % (timeit.time.perf_counter() - start))
